426eb2c144237b539ded1401a11ad37b159140e2
Jason Singh <jasonsingh@Jasons-MBP.home>
2021-01-04 20:28:21 -0500

    added section one component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header'
+import SectionOne from './Components/SectionOne'
 
 function App() {
   return (
     <div>
       <Header />
+      <SectionOne />
     </div>
   );
 }
---
+++ src/Components/SectionOne.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SectionOne = () => {
+    return(
+        <div>Section One</div>
+    )
+}
+
+export default SectionOne;
