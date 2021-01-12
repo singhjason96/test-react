152c31d5b9ab2cd0458da339f67e81affc208784
Jason Singh <jasonsingh@Jasons-MBP.home>
2021-01-12 14:20:25 -0500

    added make styles to section 1

--- src/Components/SectionOne.jsx
+++ src/Components/SectionOne.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { createApi } from 'unsplash-js';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+   sectionFlex: {
+       display: 'flex',
+       
+   },
+  }));
 
 const SectionOne = () => {
+    const classes = useStyles();
+      
+      
     return(
-        <div>Section One</div>
+        <div className={classes.sectionFlex}>
+            <div>Section One</div>
+            <div>Section 2s</div>
+        </div>
     )
 }
 
